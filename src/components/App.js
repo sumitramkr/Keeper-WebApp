935600452abf9f69cbf59a46aed4386205b84efc
sumitramkr <sumitramkr77e@gmail.com>
2022-06-28 01:03:59 +0530

    added arrow fn and multiple notes from notes.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import Header from "./Header.js";
 import Footer from "./Footer.js";
 import Note from "./Note.js";
+import notes from "../notes";
 
 function App() {
   return (
     <div>
-      <Header /> <Note /> <Footer />
+      <Header />
+      {notes.map((item) => {
+        return <Note title={item.title} content={item.content} />;
+      })}
 
+      <Footer />
     </div>
   );
 }
