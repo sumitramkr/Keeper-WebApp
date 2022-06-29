d436ac2d736b3f6ac202dbb075e7588654ff6e87
sumitramkr <sumitramkr77e@gmail.com>
2022-06-30 02:10:02 +0530

    added delete func

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,11 +5,38 @@ import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [textArray, setTextArray] = React.useState([]);
+
+  function addNote(note) {
+    setTextArray((textArray) => {
+      return [...textArray, note];
+    });
+  }
+
+  function deleteNote(id) {
+    setTextArray((textArray) => {
+      return textArray.filter((item, index) => {
+        return id !== index;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea clicked={addNote} />
+      {textArray.map((item, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={item.title}
+            content={item.content}
+            delete={deleteNote}
+          />
+        );
+      })}
+
       <Footer />
     </div>
   );
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,49 @@
 import React from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+  const [text, setText] = React.useState({
+    title: "",
+    content: "",
+  });
+
+  function handleTitle(event) {
+    const { name, value } = event.target;
+    setText((text) => {
+      return {
+        ...text,
+        [name]: value,
+      };
+    });
+  }
+
+  function submitted(event) {
+    props.clicked(text);
+    setText(() => {
+      return {
+        title: "",
+        content: "",
+      };
+    });
+    event.preventDefault();
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input
+          name="title"
+          placeholder="Title"
+          onChange={handleTitle}
+          value={text.title}
+        />
+        <textarea
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          onChange={handleTitle}
+          value={text.content}
+        />
+        <button onClick={submitted}>Add</button>
       </form>
     </div>
   );
